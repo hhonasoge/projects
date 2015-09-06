945b04dc443d911b12a0ca4338e6811772f020d0
Harsha <harsha.honasoge@gmail.com>
2015-09-06 02:27:26 -0700

    Add flipBit

--- BitManip/bitManip.java
+++ BitManip/bitManip.java
@@ -1,6 +1,6 @@
 public class bitManip {
 	public static int getBit(int num, int i){
-		int mask = i << 1;
+		int mask = 1 << i;
 		int val = num & mask;
 		return val != 0 ? 1 : 0;
 	}
@@ -22,15 +22,50 @@ public class bitManip {
 		int mask2 = (-1 >>> (32-j));
 		int mask3 = mask1 | mask2;
 		return N & mask3;
-
 	}
-	public static int insert(int N, int M, int i, int j){
+	public static int getOnesAfter(int num, int i) {
+		int count = 0;
+		while(getBit(num, i) !=0) {
+			count++;
+			i--;
+		}
+		return count;
+	}
+	public static int flipBit(int num) {
+		int maxOneCount = 0;
+		int currCount = 0;
+		int tmpCount = 0;
+		int i = 31;
+		while(i>-1){
+			int bitVal = getBit(num, i);
+			if (bitVal == 1) {
+				currCount++;
+				i--;
+			}
+			else {
+					tmpCount = getOnesAfter(num, i-1);
+					currCount +=tmpCount+1;
+					if (currCount>maxOneCount) {
+						maxOneCount = currCount;
+					}
+					currCount = tmpCount;
+					i -= (tmpCount+1);
+			}
+		}
+		if (currCount > maxOneCount) {
+			maxOneCount = currCount;
+		}
+		return maxOneCount;
+	}
+	public static int insert(int N, int M, int i, int j) {
 		int newN = clearBits(N, i, j);
 		int newM = M << i;
 		return newN | newM;
 	}
+
 	public static void main(String[] args) {
 		System.out.println(insert(1024, 19, 2, 6));
+		System.out.println(flipBit(1775));
 	}
 
 }

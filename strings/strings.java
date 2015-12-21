9243c28a305b8cdab207412cb7d3a1e43408685c
Harsha <harsha.honasoge@gmail.com>
2015-12-21 13:49:36 -0500

    isPalPermut1+isOneEditAway

--- strings/strings.java
+++ strings/strings.java
@@ -100,7 +100,94 @@ public class strings {
 		}
 		return true;
 	}
-
+	public static void URLify(char[] str, int length){
+		int spaceCount = 0;
+		int newLength;
+		for (int i=0; i<length; i++){
+			if (str[i]==' '){
+				spaceCount++;
+			}
+		}
+		newLength = length + spaceCount*2;
+		// str[newLength]='\0';
+		for (int i=length-1; i>=0;i--){
+			if (str[i]==' '){
+				str[newLength-1]='0';
+				str[newLength-2]='2';
+				str[newLength-3]='%';
+				newLength=newLength - 3;
+			} else {
+				str[newLength-1] = str[i];
+				newLength=newLength - 1;
+			}
+		}
+	}
+	public static boolean isPalPermutation1(String input){
+		HashMap<Character, Integer> charCount = new HashMap<Character, Integer>();
+		for (int i=0; i<input.length();i++){
+			if (!charCount.containsKey(input.charAt(i))||charCount.get(input.charAt(i))==0){
+				charCount.put(input.charAt(i), 1);
+			} else {
+				charCount.put(input.charAt(i),0);
+			}
+		}
+		boolean maxOneOdd = false;
+		for (Character key: charCount.keySet()){
+			if (charCount.get(key)!=0){
+				if (!maxOneOdd){
+					maxOneOdd=true;
+				} else {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	public static boolean isOneEditAway(String a, String b){
+		int alen = a.length();
+		int blen = b.length();
+		int difflen = alen-blen;
+		if (difflen>1||difflen<-1){
+			return false;
+		} else if (difflen==0){
+			return checkReplace(a, b);
+		} else if (difflen>0){
+			return checkRemove(a, b);
+		} else {
+			return checkRemove(b, a);
+		}
+	}
+	public static boolean checkReplace(String a, String b){
+		boolean maxOneDiff = false;
+		for (int i=0; i<a.length(); i++){
+			if (a.charAt(i)!=b.charAt(i)){
+				if (maxOneDiff){
+					return false;
+				} else {
+					maxOneDiff=true;
+				}
+			}
+		}
+		return true;
+	}
+	public static boolean checkRemove(String longer, String shorter){
+		int longIndex=0;
+		int shortIndex=0;
+		boolean maxOneDiff = false;
+		while(longIndex<longer.length()&&shortIndex<shorter.length()){
+			if (longer.charAt(longIndex)!=shorter.charAt(shortIndex)){
+				if (maxOneDiff){
+					return false;
+				} else {
+					longIndex++;
+				}
+			} else {
+				longIndex++;
+				shortIndex++;
+			}
+		}
+		return true;
+	}
 	public static void main(String[] args) {
 		String test="Harsha";
 		String test2 = "asdfg";
@@ -132,7 +219,37 @@ public class strings {
 		print("-------");
 		print(isPermutation2("harsha", "arshah"));
 		print(isPermutation2("asfjw", "alkfgwe"));
+		print("--------");
 
+		String test6 = "Hars  ha";
+		char[] test6_2 = new char[test6.length()+4];
+		for (int i=0;i<test6.length();i++){
+			test6_2[i]=test6.charAt(i);
+		}
+		// print(test6_2[4]);
+		// // print(test6_2.toString());
+		URLify(test6_2, 8);
+		test6= new String(test6_2);
+		System.out.println(test6_2);
+		print("---------");
+		print(isPalPermutation1("racecar"));
+		print(isPalPermutation1("abc"));
+
+		HashMap<Integer, Integer> squares = new HashMap<Integer, Integer>();
+		for (int i=0; i<25; i++){
+			squares.put(i, i*i);
+		}
+		Iterator<Map.Entry<Integer, Integer>> entries = squares.entrySet().iterator();
+		while (entries.hasNext()){
+			print(entries.next());
+		}
+		print("----------");
+		print(isOneEditAway("Harsha", "arsha"));
+		print(isOneEditAway("", "alaksuhg"));
+		print(isOneEditAway("pale", "ple"));
+		print(isOneEditAway("pales", "pale"));
+		print(isOneEditAway("pale", "bale"));
+		print(isOneEditAway("pale", "bake"));
 		// newClass a = new newClass();
 		// Class c = test.getClass();
 		// System.out.println(b instanceof String);

aa9794625c64bfa9967e3d4128de8a78d4db47cb
Harsha <harsha.honasoge@gmail.com>
2015-12-31 02:28:43 -0500

    convert int to English phrase

--- Moderate/Moderate.java
+++ Moderate/Moderate.java
@@ -42,6 +42,61 @@ public class Moderate{
 		int i = c>>31;
 		return a + i*c;
 	}
+	public static int findTrailingZeros(int nFactorial){
+		if (nFactorial<=4)return 0;
+		int count=0;
+		for (int i=nFactorial; i>4; i--){
+			count+=numFactorsOfFive(i);
+		}
+		return count;
+	}
+	public static int numFactorsOfFive(int n){
+		int count = 0;
+		while (n%5==0){
+			count++;
+			n=n/5;
+		}
+		return count;
+	}
+	public static String[] smalls = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+	public static String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+	public static String hundred = "Hundred";
+	public static String[] bigs =  {"", "Thousand", "Million", "Billion"};
+	public static String convert(int num){
+		if (num==0){
+			return smalls[0];
+		}
+		if (num<0){
+			return "Negative" + convert(-1*num);
+		}
+		String convertedString = "";
+		int bigCounter = 0;
+		while (num>0){
+			if (num%1000!=0){
+				convertedString = convertChunk(num%1000) + " "+ bigs[bigCounter] + " " + convertedString;
+				bigCounter++;
+			}
+			num /= 1000;
+		}
+		return convertedString;
+	}
+	public static String convertChunk(int num){
+		String chunkString = "";
+		if (num>=100){
+			chunkString+= smalls[num/100] + " " +hundred;
+			num %= 100;
+		}
+		if (num>=10 && num <=19){
+			chunkString+=smalls[num];
+		} else if (num >= 20){
+			chunkString+=" " + tens[num/10];
+			num%=10;
+		}
+		if (num>=0 && num<=9){
+			chunkString+=" " + smalls[num];
+		}
+		return chunkString;
+	}
 	public static void main(String[] args){
 		int[] test = {1, 2, 3, 4};
 		swap(test, 1, 2);
@@ -55,5 +110,7 @@ public class Moderate{
 		System.out.println("smallestDifferenceBrute: " + smallestDifferenceBrute(a, b));
 		System.out.println("smallestDifferenceOptimal: " + smallestDifferenceOptimal(a, b));
 		System.out.println(findMax(5, 3));
+		System.out.println(findTrailingZeros(10));
+		System.out.println(convert(-1234));
 	}
 }

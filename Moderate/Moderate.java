3c7e3d57f0ad0b1204778ab5c49e4fa99533f46e
Harsha <harsha.honasoge@gmail.com>
2016-01-05 14:20:15 -0600

    pondSizes and patternMatching

--- Moderate/Moderate.java
+++ Moderate/Moderate.java
@@ -97,6 +97,95 @@ public class Moderate{
 		}
 		return chunkString;
 	}
+	public static int findMaxSum(int[] arr){
+		int currSum, maxSum;
+		currSum=maxSum=0;
+		for (int i=0;i<arr.length;i++){
+			if (arr[i]>=0){
+				currSum+=arr[i];
+				if (currSum>maxSum){
+					maxSum=currSum;
+				}
+			} else {
+				currSum=Math.max(currSum+arr[i], 0);
+			}
+		}
+		return maxSum;
+	}
+	public static boolean matchesPattern(String value, String pattern){
+		int patternLength = pattern.length();
+		if (patternLength==0) return false;
+		char mainChar = pattern.charAt(0);
+		char altChar = mainChar == 'a' ? 'b' : 'a';
+		int countMain = countOf(pattern, mainChar);
+		int countAlt = patternLength - countMain;
+		int valueSize = value.length();
+		int maxMainSize = valueSize/countMain;
+		int firstAlt = pattern.indexOf(altChar);
+		for (int mainSize =0; mainSize < maxMainSize; mainSize++){
+			int remainingLength = valueSize - mainSize*countMain;
+			String first = value.substring(0, mainSize);
+			if (countAlt==0||remainingLength%countAlt==0){
+				int altSize = countAlt==0 ? 0 : remainingLength/countAlt;
+				int altIndex = firstAlt*mainSize;
+				String second = countAlt==0 ? "" : value.substring(altIndex, altSize+altIndex);
+				String cand = buildFromPattern(pattern, first, second);
+				if (cand.equals(value)){
+					return true;
+				}
+			}
+
+		}
+		return false;
+
+	}
+	public static String buildFromPattern(String pattern, String first, String second){
+		StringBuffer sb = new StringBuffer();
+		char firstChar = pattern.charAt(0);
+		for (char c : pattern.toCharArray()){
+			if (c==firstChar){
+				sb.append(first);
+			} else {
+				sb.append(second);
+			}
+		}
+		return sb.toString();
+	}
+	public static int countOf(String pattern, char mainChar){
+		int count=0;
+		for (int i=0; i<pattern.length(); i++){
+			if (pattern.charAt(i)==mainChar){
+				count++;
+			}
+		}
+		return count;
+	}
+	public static ArrayList<Integer> computePondSizes(int[][] land){
+		ArrayList<Integer> pondSizes = new ArrayList<Integer>();
+		for (int i=0; i<land.length; i++){
+			for (int j=0; j<land[0].length; j++){
+				int size = computeSize(land, i, j);
+				if (size>0){
+					pondSizes.add(size);
+				}
+			}
+		}
+		return pondSizes;
+	}
+	public static int computeSize(int[][] land, int row, int col){
+		if(row<0||col<0||row>(land.length-1)||col>(land[0].length-1)||land[row][col]==-1||land[row][col]>0) return 0;
+		land[row][col]=-1;
+		int size = 1;
+		size+=computeSize(land, row-1, col);
+		size+=computeSize(land, row-1, col-1);
+		size+=computeSize(land, row-1, col+1);
+		size+=computeSize(land, row, col-1);
+		size+=computeSize(land, row, col+1);
+		size+=computeSize(land, row+1, col-1);
+		size+=computeSize(land, row+1, col);
+		size+=computeSize(land, row+1, col+1);
+		return size;
+	}
 	public static void main(String[] args){
 		int[] test = {1, 2, 3, 4};
 		swap(test, 1, 2);
@@ -112,5 +201,14 @@ public class Moderate{
 		System.out.println(findMax(5, 3));
 		System.out.println(findTrailingZeros(10));
 		System.out.println(convert(-1234));
+		int[] arr = {2, -8, 3, -20, 0, 10};
+		System.out.println(findMaxSum(arr));
+		System.out.println(matchesPattern("catcatdogcat", "aaba"));
+		int[][] land = {{0, 2, 1, 0}, {0, 1, 0, 1}, {1, 1, 0, 1}, {0, 1, 0, 1}};
+		ArrayList<Integer> pondSizes = computePondSizes(land);
+		System.out.println(pondSizes.size());
+		for (int i=0; i<pondSizes.size(); i++){
+			System.out.print(pondSizes.get(i) + " ");
+		}
 	}
 }
